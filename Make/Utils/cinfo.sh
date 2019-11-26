f39e60a193796df1482bef15bccdf920969f2bd9
Michele Mesiti <michele.mesiti@swansea.ac.uk>
2019-11-26 18:02:36 +0000

    cinfo.sh sanboxed for make -j N
    when building with make -j N, cinfo.sh is called many times and there is no
    lock on cinfo.c. Found workaround. Better would be to fix MkRules.

--- Make/Utils/cinfo.sh
+++ Make/Utils/cinfo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-FILENAME=cinfo.c
+TMPFILENAME=cinfo.tmp.$BASHPID
+FILENAME=cinfo.c.$BASHPID
+FILENAME_TRUE=cinfo.c
 MKDIR=$1
 TOPDIR=$2
 PWD=`pwd`
@@ -8,76 +10,77 @@ PWD=`pwd`
 shift; shift
 MACROS=$@
 
+
 echo "// created automatically by $0 on $(date)" > ${FILENAME}
 
 
-echo -n ${MACROS} | tr '"' '@' > cinfo.tmp
-len=$((`cat cinfo.tmp | wc -c`+1))
+echo -n ${MACROS} | tr '"' '@' > $TMPFILENAME
+len=$((`cat $TMPFILENAME | wc -c`+1))
 echo -n "static char MACROS[${len}] = \"" >> ${FILENAME}
-cat cinfo.tmp >> ${FILENAME}
+cat $TMPFILENAME >> ${FILENAME}
 echo "\";" >> ${FILENAME}
 echo "" >> ${FILENAME}
-rm cinfo.tmp
+rm $TMPFILENAME
 
-awk '{printf "%s\\n",$0}' ${MKDIR}/MkFlags > cinfo.tmp
-len=$((`cat cinfo.tmp | wc -c`+1))
+awk '{printf "%s\\n",$0}' ${MKDIR}/MkFlags > $TMPFILENAME
+len=$((`cat $TMPFILENAME | wc -c`+1))
 echo -n "static char CI_mkflags[${len}] = \"" >> ${FILENAME}
-cat cinfo.tmp >> ${FILENAME}
+cat $TMPFILENAME >> ${FILENAME}
 echo "\";" >> ${FILENAME}
 echo "" >> ${FILENAME}
-rm cinfo.tmp
+rm $TMPFILENAME
 
 if [[ -a /proc/cpuinfo ]] 
 then
- awk '{printf "%s\\n",$0}' /proc/cpuinfo > cinfo.tmp
+ awk '{printf "%s\\n",$0}' /proc/cpuinfo > $TMPFILENAME
 else
- echo -n "No CPU info\n" > cinfo.tmp
+ echo -n "No CPU info\n" > $TMPFILENAME
 fi
-len=$((`cat cinfo.tmp | wc -c`+1))
+len=$((`cat $TMPFILENAME | wc -c`+1))
 echo -n "static char CI_cpuinfo[${len}] = \"" >> ${FILENAME}
-cat cinfo.tmp >> ${FILENAME}
+cat $TMPFILENAME >> ${FILENAME}
 echo "\";" >> ${FILENAME}
 echo "" >> ${FILENAME}
-rm cinfo.tmp
+rm $TMPFILENAME
 
 if [[ -a /proc/version ]] 
 then
- awk '{printf "%s\\n",$0}' /proc/version > cinfo.tmp
+ awk '{printf "%s\\n",$0}' /proc/version > $TMPFILENAME
 else
- echo -n "No VERSION info\n" > cinfo.tmp
+ echo -n "No VERSION info\n" > $TMPFILENAME
 fi
-len=`cat cinfo.tmp | wc -c`+1
+len=`cat $TMPFILENAME | wc -c`+1
 echo -n "static char CI_linux[${len}] = \"" >> ${FILENAME}
-cat cinfo.tmp >> ${FILENAME}
+cat $TMPFILENAME >> ${FILENAME}
 echo "\";" >> ${FILENAME}
 echo "" >> ${FILENAME}
-rm cinfo.tmp
+rm $TMPFILENAME
 
-gcc -v 2>&1 | awk '{printf "%s\\n",$0}' > cinfo.tmp
-len=`cat cinfo.tmp | wc -c`+1
+gcc -v 2>&1 | awk '{printf "%s\\n",$0}' > $TMPFILENAME
+len=`cat $TMPFILENAME | wc -c`+1
 echo -n "static char CI_gcc[${len}] = \"" >> ${FILENAME}
-cat cinfo.tmp >> ${FILENAME}
+cat $TMPFILENAME >> ${FILENAME}
 echo "\";" >> ${FILENAME}
 echo "" >> ${FILENAME}
-rm cinfo.tmp
+rm $TMPFILENAME
 
 #svn --version >&- ; ret=$?
 #if [ "${ret}" -eq "0" ] ; then
-#  svn info ${TOPDIR} | awk '{printf "%s\\n",$0}' > cinfo.tmp
-#  len=`cat cinfo.tmp | wc -c`+1
+#  svn info ${TOPDIR} | awk '{printf "%s\\n",$0}' > $TMPFILENAME
+#  len=`cat $TMPFILENAME | wc -c`+1
 #  echo -n "static char CI_svninfo[${len}] = \"" >> ${FILENAME}
-#  cat cinfo.tmp >> ${FILENAME}
+#  cat $TMPFILENAME >> ${FILENAME}
 #  echo "\";" >> ${FILENAME}
 #  echo "" >> ${FILENAME}
-#  rm cinfo.tmp
+#  rm $TMPFILENAME
 
-#  svn st -q ${TOPDIR} | awk '{printf "%s\\n",$0}' > cinfo.tmp
-#  len=`cat cinfo.tmp | wc -c`+1
+#  svn st -q ${TOPDIR} | awk '{printf "%s\\n",$0}' > $TMPFILENAME
+#  len=`cat $TMPFILENAME | wc -c`+1
 #  echo -n "static char CI_svnstatus[${len}] = \"" >> ${FILENAME}
-#  cat cinfo.tmp >> ${FILENAME}
+#  cat $TMPFILENAME >> ${FILENAME}
 #  echo "\";" >> ${FILENAME}
 #  echo "" >> ${FILENAME}
-#  rm cinfo.tmp
+#  rm $TMPFILENAME
 #else
   echo -n "static char CI_svninfo[1] = \"\";" >> ${FILENAME}
   echo "" >> ${FILENAME}
@@ -92,4 +95,6 @@ rm cinfo.tmp
 echo "static int CI_svnrevision = ${REV};" >> ${FILENAME}
 echo "" >> ${FILENAME}
 
-cat ${MKDIR}/Utils/${FILENAME}.tmpl >> ${FILENAME}
+cat ${MKDIR}/Utils/${FILENAME_TRUE}.tmpl >> ${FILENAME}
+
+mv $FILENAME $FILENAME_TRUE
